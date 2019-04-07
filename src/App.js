84af69245db59423f947c38392f3e35947e806d3
Sourangsu Mukherjee <sourangsumukherjee@Sourangsus-MacBook-Air.local>
2019-04-07 23:35:57 +0530

    Routing Applied

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import Movies from "./component/movies";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Rentals from "./component/rentals";
+import Customers from "./component/customers";
+import NotFound from "./component/notFound";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <main className="App">
-        <h1>Hello World</h1>
-        <Movies />
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
     );
   }
