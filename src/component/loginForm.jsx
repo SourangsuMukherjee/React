6d5e175f8d3dc100901b81663bee85144c520048
Sourangsu Mukherjee <sourangsumukherjee@Sourangsus-MacBook-Air.local>
2019-04-13 01:53:41 +0530

    Login Form fixed submit button

--- src/component/loginForm.jsx
+++ src/component/loginForm.jsx
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    acount: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const username = this.username.current.value;
+    console.log("Submitted", username);
+  };
+
   render() {
     return (
       <div>
@@ -9,17 +22,22 @@ class LoginForm extends Component {
           <div className="row">
             <div className="col-md-4 col sm-4 col-xs-12" />
             <div className="col-md-4 col sm-4 col-xs-12">
-              <form className="form-container">
+              <form className="form-container" onSubmit={this.handleSubmit}>
                 <div className="form-group">
                   <label htmlFor="username">Username</label>
-                  <imput id="username" className="form-control" />
+                  <input
+                    autoFocus
+                    value={this.state.ac}
+                    id="username"
+                    className="form-control"
+                  />
                   <small className="form-text text-muted">
                     We'll never share your email with anyone else.
                   </small>
                 </div>
                 <div className="form-group">
                   <label htmlFor="password">Password</label>
-                  <imput id="password" className="form-control" />
+                  <input id="password" className="form-control" />
                 </div>
                 <button className="btn btn-success btn-block">Login</button>
               </form>
